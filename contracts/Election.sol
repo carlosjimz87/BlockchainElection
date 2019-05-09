fffd7ae1c41b1b99de53553866dc7017a3274d1a
Carlos Jimenez <carlosjimz87@gmail.com>
2019-05-09 16:49:23 +0200

    Adding election contract and tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,12 +1,25 @@
 pragma solidity ^0.5.0;
 
 contract Election{
+    // model a candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    //store candidates
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
     
-    string public candidate;
-
     // Constructor
+    constructor  () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
 
-    constructor  () public{
-        candidate = "Candidate 1";
+    function addCandidate(string memory _name) private{
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
